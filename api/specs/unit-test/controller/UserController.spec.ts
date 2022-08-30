aa9eb5668f4ce62d98ca0d15ada7bba1f43da54c
Jiadong Chen <jiadchen@redhat.com>
2022-08-30 11:09:01 +0900

    UserControllerのunit testを追加

---
+++ api/specs/unit-test/controller/UserController.spec.ts
@@ -0,0 +1,17 @@
+import { UserController } from "../../../src/controller/UserController";
+
+describe("UserController Tests", () => {
+  test("normal case", () => {
+    // Arrange
+    const expected = JSON.stringify([
+      { id: 1, name: "User1", email: "user1@test.local" },
+      { id: 2, name: "User2", email: "user2@test.local" },
+      { id: 3, name: "User3", email: "user3@test.local" },
+    ]);
+    // Act
+    const target = new UserController();
+    const actual = target.get({} as any);
+    // Assert
+    expect(actual).toBe(expected);
+  });
+});
