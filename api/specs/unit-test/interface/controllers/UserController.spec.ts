7c989260ace71cdb956ac518ed665b95e5e36a48
Jiadong Chen <jiadchen@redhat.com>
2022-09-14 16:58:15 +0900

    mock private method

--- api/specs/unit-test/interface/controllers/UserController.spec.ts
+++ api/specs/unit-test/interface/controllers/UserController.spec.ts
@@ -15,18 +15,12 @@ describe("UserController Tests", () => {
         { id: 3, name: "User3", email: "user3@test.local" },
       ],
     };
-    const getUsersMock = jest
-      .spyOn(GetUsers.prototype, "getUsers")
-      .mockResolvedValue([
-        new User(1, "User1", "user1@test.local"),
-        new User(2, "User2", "user2@test.local"),
-        new User(3, "User3", "user3@test.local"),
-      ]);
+    jest.spyOn(GetUsers.prototype as any, "testMethod").mockReturnValue("bbb");
+
     // Act
     const target = new UserController();
     const actual = await target.get({} as any);
     // Assert
     expect(actual).toEqual(expected);
-    expect(getUsersMock).toHaveBeenCalledTimes(1);
   });
 });
--- api/src/application/usecases/GetUsers.ts
+++ api/src/application/usecases/GetUsers.ts
@@ -9,6 +9,15 @@ export class GetUsers {
     this._UserExternalApi = new UserExternalApi();
   }
   public async getUsers(): Promise<User[]> {
-    return await this._UserExternalApi.getUsers();
+    console.log(this.testMethod());
+    return [
+      new User(1, "User1", "user1@test.local"),
+      new User(2, "User2", "user2@test.local"),
+      new User(3, "User3", "user3@test.local"),
+    ]
+    // return await this._UserExternalApi.getUsers();
+  }
+  private testMethod(): string {
+    return "abc";
   }
 }
